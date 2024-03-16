5544d22b3e9e5e1cefc1e533dd2dd055313e0879
ZeroJChang <55027688+ZeroJChang@users.noreply.github.com>
2024-03-15 18:30:55 -0600

    Hola Mundo

---
+++ JenkinsHolaMundo/JenkinsHolaMundo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JenkinsHolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
